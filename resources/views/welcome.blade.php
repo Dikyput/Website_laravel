558c78395cb878b085d0874b82194a21b285fb2e
Dikyput <joykey.grapichdiky@gmail.com>
2022-03-14 14:51:13 +0700

    laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.main')
+
+
+@section('title', 'Belajar - PERPUSTAKAAN')
+
+
+@section('content')
+
+
+<div class="container">
+
+
+<div class="jumbotron">
+<h1 class="display-4">Selamat Datang!</h1>
+<p class="lead">PERPUSTAKAAN MAHASISWA</p>
+<hr class="my-4">      
+</div>
+
+
+</div>
+
+
+@endsection
